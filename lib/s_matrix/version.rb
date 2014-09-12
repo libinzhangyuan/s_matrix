6bfc1ff388bd97a3b4fa31ca9af640e961f71a5e
zhangyuan <libinzhangyuan@gmail.com>
2014-09-12 09:56:38 +0800

    zhangyuan master   add rspec construction

--- lib/s_matrix/version.rb
+++ lib/s_matrix/version.rb
@@ -1,3 +1,3 @@
 module SMatrix
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
---
+++ spec/smatrix_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe SMatrix do
+  describe 'test' do
+    it '' do
+      expect(SMatrix.test).to eq(3)
+    end
+  end
+end
